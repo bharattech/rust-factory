888cc0c7ec387eceeed4a0e1f4ba82bd37b54d20
bharattech <bharattech@protonmail.com>
2023-06-29 18:24:53 +0530

    trait

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
 fn main() {
-    struct Book {
-        title: String,
-        isbn: Option<String>,
-    }
-    let book = Book {
-        title: "Great book".to_string(),
-        isbn: Some(String::from("1-123-456")),
-    };
-    match book.isbn {
-        Some(i) => println!("The ISBN of the book: {} is: {}", book.title, i),
-        None => println!("We don't know the ISBN of the book"),
-    }
+    let question = Question::new(
+        QuestionId("1".to_string()), 
+        "First Question".to_string(), 
+        "Content of question".to_string(), 
+        Some(vec!("faq".to_string())));
+        println!("{:?}", question);
 }
